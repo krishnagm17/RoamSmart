1048c52a88838e046e281084110a580fe5c6e38c
Movie Ticket Booking System <krishnametri223344@gmail.com>
2026-08-18 14:42:44 +0530

    chore: temporarily nuke all groups on load

--- frontend/src/components/roamgroups/RoamGroupsScreen.jsx
+++ frontend/src/components/roamgroups/RoamGroupsScreen.jsx
@@ -452,6 +452,9 @@ function uid2() {
 }
 
 function GroupsHome({ groups, self, notifs, unreadMap, onCreate, onJoin, onOpen, onNotifs, unreadCount }) {
+  useEffect(() => {
+    groups.forEach(g => deleteGroup(g.gid));
+  }, [groups]);
   return (
     <div className="rg-groups-pad">
       <div className="rg-head">
